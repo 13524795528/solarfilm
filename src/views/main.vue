62d180775101583af440c2b5fc1ae77d78a92a8c
Ray.Z <ray.z@RayZdeMacBook-Pro.local>
2021-01-21 17:14:10 +0800

    20210121

--- src/views/main.vue
+++ src/views/main.vue
@@ -12,28 +12,47 @@
                 <span>{{$store.getters.getUser.usercode}}</span>
             </el-header>
         </el-container>
-        <el-container style="height:100%; border:1px solid #eee">
+        <el-container style="height:100%; border:1px solid #eeeeee">
             <el-aside width="200px" style="background-color: rgb(238,241,246)">
-                <el-menu>
-                    <el-submenu index="1">
-                        <template slot="title">
-                            <i class="el-icon-message"></i>客户资料维护
-                        </template>
-                        <el-menu-item-group>
-                            <el-menu-item index="1-1"><router-link to="/main/client/1">用户ID=1</router-link></el-menu-item>
-                            <el-menu-item index="1-2"><router-link :to="{name:'client', params:{id:2}}">用户ID=2</router-link></el-menu-item>
-                        </el-menu-item-group>
-                    </el-submenu>
-                    <el-submenu index="2">
-                        <template slot="title">
-                            <i class="el-icon-message"></i>PDI车辆处理
-                        </template>
-                        <el-menu-item-group>
-                            <el-menu-item index="2-1"><router-link :to="{name:'PDI', params: {id:3}}">PDI ID=3</router-link></el-menu-item>
-                            <el-menu-item index="2-2"><router-link to="/gomain">回到首页</router-link></el-menu-item>
-                        </el-menu-item-group>
-                    </el-submenu>
-                </el-menu>
+                    <el-menu
+                            default-active="2"
+                            class="el-menu-vertical-demo"
+                            @open="handleOpen"
+                            @close="handleClose"
+                            background-color="#545c64"
+                            text-color="#fff"
+                            active-text-color="#ffd04b">
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template slot="title">分组一</template>
+                                <el-menu-item index="1-1"><router-link :to="{name:'PDI', params: {id:3}}">PDI ID=3</router-link></el-menu-item>
+                                <el-menu-item index="1-2"><router-link to="/gomain">回到首页</router-link></el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                                <el-menu-item index="1-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="1-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="1-4-1">选项1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title"><router-link :to="{name:'client', params:{id:2}}">用户ID=2</router-link></span>
+                        </el-menu-item>
+                        <el-menu-item index="3" disabled>
+                            <i class="el-icon-document"></i>
+                            <span slot="title">导航三</span>
+                        </el-menu-item>
+                        <el-menu-item index="4">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">导航四</span>
+                        </el-menu-item>
+                    </el-menu>
             </el-aside>
             <el-main>
                 <p>我是main内容框</p>
